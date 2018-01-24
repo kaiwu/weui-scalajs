31c8aa29c5567a5fda506a1ca32ba017004bf7a8
wukai <kai.wu@goodbaby.com>
2018-01-24 22:21:49 +0800

    add wx functions

--- common/src/main/scala/wechat.scala
+++ common/src/main/scala/wechat.scala
@@ -5,13 +5,19 @@ import scala.scalajs.js.annotation._
 @JSGlobalScope
 object wechatapp extends js.Object {
   @JSName("App")
-  def App (obj: js.Object): Unit = js.native
+  def App (o: js.Object): Unit = js.native
 
   @JSName("Page")
-  def Page (obj: js.Object): Unit = js.native
+  def Page (o: js.Object): Unit = js.native
 }
 
 @js.native
 @JSGlobal("wx")
 object wx extends js.Object {
+  def login (o: js.Object): Unit = js.native
+  def getUserInfo (o: js.Object): Unit = js.native
+  def getStorageSync (key: String): js.Object = js.native
+  def setStorageSync (key: String, data: js.Object): Unit = js.native
+  def navigateTo(o: js.Object): Unit = js.native
+
 }
